14bd5ef35ff8d943b1a1286e22ddf9727033a5f2
sannimdev <inegg.apps@gmail.com>
2022-03-19 21:10:25 +0900

    2022-03-19 복습6

---
+++ src/복습/2022-03/029-너비우선탐색.js
@@ -0,0 +1,17 @@
+function solution(arg) {
+    let answer = '';
+    const queue = [];
+    queue.push(1);
+
+    while (queue.length) {
+        const v = queue.shift();
+        answer += v + ' ';
+        for (let nv of [v * 2, v * 2 + 1]) {
+            if (nv > 7) continue;
+            queue.push(nv);
+        }
+    }
+
+    return answer;
+}
+console.log(solution());
